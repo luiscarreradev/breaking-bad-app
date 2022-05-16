cca366ada65305325e59b709a346a7f4661e693a
Luis Carrera <luiscarreramv@gmail.com>
2022-05-16 06:27:56 -0600

    Finalizado primera parte

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+import Quote from "./components/Quote"
+import Spinner from "./components/Spinner"
 
 const App = () => {
+
+  const initialQuote = {
+    text: 'Quote',
+    author: 'Autor'
+  }
+
+  const [quote, setQuote] = useState(initialQuote)
+
+  const [loading, setLoading] = useState(true)
+
+  const updateQuote = async () => {
+    setLoading(true);
+    const url = 'https://breakingbadapi.com/api/quote/random'
+    const res = await fetch(url)
+    const [newQuote] = await res.json()
+    const {quote: text, author} = newQuote
+
+    setQuote({
+      text,
+      author
+    })
+    
+    setLoading(false)
+  }
+
+
+  useEffect(() => {
+    updateQuote()
+  }, [])
+
+
   return (
-    <div>App</div>
+    <div className="app">
+      <img 
+        src="https://upload.wikimedia.org/wikipedia/commons/7/77/Breaking_Bad_logo.svg"
+        alt="logo"
+      />
+      <button onClick={updateQuote}>Get Another</button>
+      {loading ? <Spinner/> : <Quote quote={quote} />}
+    </div>
   )
 }
 
---
+++ src/components/Spinner.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import '../styles/Spinner.css';
+
+const Spinner = () => {
+  return (
+    <div className="lds-default">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Spinner;
